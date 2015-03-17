c5d2e5d14b5f95cc6b1fa997198644efce91c818
Vladimir Makarenko <vladimir@makarenko.io>
2015-03-17 21:51:24 +0300

    trains etc/

--- src/Train.java
+++ src/Train.java
@@ -3,46 +3,74 @@ public class Train {
     Wagon first = null;
 
     @Override
-    public String toString(){
+    public String toString() {
         String str = "//[]+++[]]-";
         Wagon cur = first;
-        while(cur != null){
+        while (cur != null) {
             str += cur.toString();
-            cur = cur.next;   // здесь застревали навсегда в бесконечном цикле, т.к. не перемещал указаатель
+            cur = cur.next;
         }
         return str;
     }
 
-    public void add(Wagon wagon){
-        if(first==null){
-            first=wagon;
-        }else{
-            Wagon current=first;
-            while (current.next!=null){
-                current=current.next;
+    public void add(Wagon wagon) {
+        if (!wagon.isFree()) {
+            throw new RuntimeException("Вагон уже прицеплен!");
+        }
+        if (first == null) {
+            first = wagon;
+        } else {
+            Wagon last = getLast();
+            last.next = wagon;
+            wagon.prev = last;
+        }
+        wagon.setFree(false);
+    }
+
+    public Wagon remove() {
+        if (first == null) {
+            return null;
+        } else {
+            if (first.next == null) {
+                Wagon wagon = first;
+                first = null;
+                wagon.setFree(true);
+                return wagon;
+            } else {
+                Wagon last = getLast();
+                last.prev.next = null;
+                last.prev = null;
+                last.setFree(true);
+                return last;
             }
-            current.next=wagon;
-            wagon.prev=current;
         }
     }
 
-    public Wagon remove(){
-        if(first==null){
+    private Wagon getLast() {
+        if (first == null) {
             return null;
-        }else{
-            if(first.next==null){
-                Wagon wagon=first;
-                first=null;
-                return wagon;
-            }else{
-                Wagon current=first;
-                while (current.next!=null){
-                    current=current.next;
-                }
-                current.prev.next=null;
-                current.prev=null;
-                return current;
-            }
         }
+        Wagon current = first;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public int board(int passCount) {
+        if (first == null) {
+            throw new RuntimeException("Вагонов нет!");
+        }
+        Wagon current = first;
+        while (current != null && passCount > 0) {
+            if (!current.isFull()) {
+             if (passCount>current.getFreeSits()){
+                 passCount=passCount-
+             }
+            }
+            current.people
+                    current = current.next;
+        }
+        return -1;
     }
 }
--- src/Wagon.java
+++ src/Wagon.java
@@ -1,14 +1,34 @@
 public class Wagon {
-    private int people=0;
+    public int people = 0;
+    private static final int MAX_PEOPLE = 50;
+    private boolean free = true;
     private String name;
     Wagon next = null;
     Wagon prev = null;
-    public Wagon(String name){
-        this.name=name;
+
+    public Wagon(String name) {
+        this.name = name;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "-[..==" + name + "==..]-";
     }
+
+    public boolean isFree() {
+        return free;
+    }
+
+    public void setFree(boolean free) {
+        this.free = free;
+    }
+
+    public boolean isFull(){
+        return people==MAX_PEOPLE;
+    }
+
+    public int getFreeSits(){
+        return MAX_PEOPLE-people;
+    }
+
 }
